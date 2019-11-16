25601d5ed504db311709547ecd90175dec16214e
toosure9 <44296129+toosure9@users.noreply.github.com>
2019-11-16 20:29:50 +0530

    Chat messages Visible

--- JS/chat.js
+++ JS/chat.js
@@ -3,7 +3,7 @@
     var search_params = new URLSearchParams(qs);
     var id = parseInt(search_params.get('id'));
    // initialInput = createInput("initials");
-
+	
 
     function save_chat(){
     firebase.auth().onAuthStateChanged(function(user) {
@@ -21,8 +21,31 @@
         t2s: true,
         t_id: id,
         timestamp: Date.now()
-    })
-    
-    //var mystr=window.location
-    
+	});
+	    
+}
+
+db.collection('chats').get().then(snapshot => {
+    setupchats(snapshot.docs);
+  });
+
+const chatList = document.querySelector('.chats');
+const setupchats = (data) => {
+		
+    let ctml = '';
+ 	data.forEach(doc => {
+	
+    const chats = doc.data();
+    console.log(chats.Message);
+    const li = `
+      <li>
+        <div class="collapsible-header grey lighten-4"> ${chats.Email} </div></a>
+        <div class="collapsible-body white"> ${chats.Message} </div>
+        
+      </li>
+    `;
+    ctml += li;
+	
+ 	});
+  chatList.innerHTML = ctml;
 }
