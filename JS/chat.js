dd225c1c3e1261737756230bb290984c2833f94d
Yashit Garg <yashitgarg@gmail.com>
2019-11-16 18:55:08 +0530

    Email problem

--- JS/chat.js
+++ JS/chat.js
@@ -1,12 +1,28 @@
-var ref = database.ref('chats');
-function save_chat(){
-var data= {
-	MIS: 111715051,
-	Message: "aa gaya message",
-	t2s: true,
-	t_id: 101,
-	timestamp: 0
-}
-ref.push(data);
-console.log("hi");
+    var semail;
+    var qs = window.location.search;
+    var search_params = new URLSearchParams(qs);
+    var id = parseInt(search_params.get('id'));
+    
+
+
+    function save_chat(){
+    firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    semail = user;
+  } else {
+    // No user is signed in.
+  }
+});
+    //console.log(semail.email); 
+    //console.log(id);
+    db.collection('chats').add({
+        EEmail: toString(semail.email),
+        Message: "6:54 ho rhe hain",
+        t2s: true,
+        t_id: id,
+        timestamp: Date.now()
+    })
+    
+    //var mystr=window.location
+    
 }
